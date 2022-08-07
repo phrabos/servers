537b5a5acea8c58b52f779652199cca75b19ab02
Patrick <phrabos@protonmail.com>
2022-08-07 11:44:15 -0700

    setup Go server

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module servers/go
 
 go 1.19
+
+require github.com/joho/godotenv v1.4.0 // indirect
--- go/main.go
+++ go/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Print("go initialized")
+	var envs map[string]string
+	envs, err := godotenv.Read(".env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := envs["PORT"]
+
+	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("online"))
+	})
+	http.ListenAndServe(port, nil)
 }
